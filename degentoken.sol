62f69579a9a0d19a7a27bba730ae79358320746d
AkT-Singh101 <126188221+AkT-Singh101@users.noreply.github.com>
2024-10-02 16:03:44 +0530

    Update degentoken.sol

--- degentoken.sol
+++ degentoken.sol
@@ -1,43 +1,45 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
 contract DegenToken is ERC20, ERC20Burnable, Ownable {
-    // Arrays for storing game items and their prices
-    string[] public gameItems;
-    uint256[] public gamePrices;
+    struct GameItem {
+        string name;
+        uint256 price;
+    }
+    
+    GameItem[] private _gameItems;
+    
+    // Mapping to keep track of redeemed items for each user
+    mapping(address => mapping(uint256 => uint256)) public redeemedItems;
 
     constructor(address initialOwner)
         ERC20("DegenToken", "DGN")
         Ownable(initialOwner)
     {
         // Adding game items and their prices in tokens
-        gameItems.push("Excalibur");//id=0
-        gameItems.push("Dragon Armor");//id=1
-        gameItems.push("Magic Potion");//id=2
-        gameItems.push("Hammer of Thor");//id=3
-
-        gamePrices.push(200);// Price for Excalibur
-        gamePrices.push(150);// Price for Dragon Armor
-        gamePrices.push(50);// Price for Magic Potion
-        gamePrices.push(300);// Price for Hammer of Thor
+        _gameItems.push(GameItem("Excalibur", 200));
+        _gameItems.push(GameItem("Dragon Armor", 150));
+        _gameItems.push(GameItem("Magic Potion", 50));
+        _gameItems.push(GameItem("Hammer of Thor", 300));
     }
 
     function redeem(uint256 itemId) public {
-        require(itemId < gameItems.length, "Item does not exist");
-
-        // Ensure the sender has enough tokens to redeem the item
-        uint256 price = gamePrices[itemId];
+        require(itemId < _gameItems.length, "Item does not exist");
+        uint256 price = _gameItems[itemId].price;
         require(balanceOf(msg.sender) >= price, "Insufficient balance");
 
         // Burn tokens in exchange for the game item
         _burn(msg.sender, price);
 
+        // Record the redemption
+        redeemedItems[msg.sender][itemId]++;
+
         // Emit a log of redemption
-        emit Redeemed(msg.sender, gameItems[itemId], price);
+        emit Redeemed(msg.sender, _gameItems[itemId].name, price);
     }
 
     // Event to log the redemption of items
@@ -47,6 +49,7 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
     function decimals() public pure override returns (uint8) {
         return 0; 
     }
@@ -55,15 +58,55 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
         return balanceOf(msg.sender);
     }
 
-    function getGameItem(uint256 index) public view returns (string memory) {
-        require(index < gameItems.length, "Item index out of bounds");
-        return gameItems[index];
-    }
-
     function transfer(address recipient, uint256 amount) public override returns (bool) {
         require(balanceOf(msg.sender) >= amount, "You do not have enough Degen Tokens");
         _transfer(msg.sender, recipient, amount);
         emit Transfer(msg.sender, recipient, amount);
         return true;
+    }
+
+    function getAvailableItems() public view returns (string[] memory) {
+        string[] memory items = new string[](_gameItems.length);
+        for (uint i = 0; i < _gameItems.length; i++) {
+            items[i] = string(abi.encodePacked(
+                "ID: ", uint2str(i), " - ",
+                _gameItems[i].name, " -> ",
+                uint2str(_gameItems[i].price)
+            ));
+        }
+        return items;
+    }
+
+    function getInventory(address user) public view returns (string[] memory) {
+        string[] memory inventory = new string[](_gameItems.length);
+        for (uint i = 0; i < _gameItems.length; i++) {
+            inventory[i] = string(abi.encodePacked(
+                "ID: ", uint2str(i), " - ",
+                _gameItems[i].name, " -> ",
+                uint2str(redeemedItems[user][i])
+            ));
+        }
+        return inventory;
+    }
+
+    // Helper function to convert uint to string
+    function uint2str(uint256 _i) internal pure returns (string memory str) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 length;
+        while (j != 0) {
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        j = _i;
+        while (j != 0) {
+            bstr[--k] = bytes1(uint8(48 + j % 10));
+            j /= 10;
+        }
+        str = string(bstr);
     }
 }
